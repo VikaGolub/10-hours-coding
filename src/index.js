005902cd713a922df55e27bd2cdc22b074448d75
Victoria Holub <victoria_holub@epam.com>
2022-01-18 11:41:21 +0200

    Basics. Part1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
+// camelCase for html attribute
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return (
+    <React.Fragment>
+      <>
+        <div>
+          <p>Hello World</p>
+        </div>
+        <div>Section2</div>
+      </>
+    </React.Fragment>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("p", {}, "Hello World")
+//   );
+// };
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
