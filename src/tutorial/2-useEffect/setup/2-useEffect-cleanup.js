ef04134a3499eff7d6f6a720383da92f4931e2dd
Victoria Holub <victoria_holub@epam.com>
2022-01-20 19:48:46 +0200

    UseEffect, dependence array, async await, cleanup, addEventListeners, removeEventListener

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
+  // console.log(window.innerWidth); // залежить від ширини браузера (чи на весь екран розкритий, чи на якусь частину екрану)
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size}</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
