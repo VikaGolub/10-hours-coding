1fb826bb564df67a6e290a3a3dd396d21190eb5a
Victoria Holub <victoria_holub@epam.com>
2022-01-21 17:24:43 +0200

    short-circuit evaluation (operation with || and &&)

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
-// short-circuit evaluation
+import React, { useState } from "react";
+// short-circuit evaluation (operation with || and &&)
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("ds");
+  const [isError, setIsError] = useState(false);
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h2>firstValue: {firstValue}</h2>
+      <h2>secondValue: {secondValue}</h2>
+      {/* if text is true - return text, is text false return 2nd variant */}
+      <h1>{text || "John"}</h1>
+      {/* if text true - return 2nd variant, if text false, don\'t return anything */}
+      {text && <h2>Monika</h2>}
+      {/* if text is false - return 2nd variant if true - return nothing */}
+      {!text && <h2>Richard</h2>}
+
+      <div>
+        <button
+          className="btn"
+          style={{ marginBottom: 10 }}
+          onClick={() => setIsError(!isError)}
+        >
+          Toggle Error
+        </button>
+
+        {isError ? <h3>There's error</h3> : <h3>There's no error</h3>}
+      </div>
+    </>
+  );
 };
 
 export default ShortCircuit;
